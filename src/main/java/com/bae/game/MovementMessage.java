d1d4398cfac3031a60e0c34e20341667353b4b15
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-06 14:33:26 +0000

    After Submission, prints movement info, refactored

--- src/main/java/com/bae/game/MovementMessage.java
+++ src/main/java/com/bae/game/MovementMessage.java
@@ -3,11 +3,11 @@ package com.bae.game;
 public class MovementMessage {
 	
 	public String playerOutOfBounds() {
-		return "The fog closes in around you... \nAs it fades away you realise you are now back where you first entered the swamp...\n";
+		return "\nThe fog closes in around you... \nAs it fades away you realise you are now back where you first entered the swamp...";
 	}
 	
 	public String movementSuccess(String dir, int distance) {
-		return "You have moved " + distance + "m " + dir + ".\n";
+		return "\nYou have moved " + distance + "m " + dir + ".";
 	}
 
 }
